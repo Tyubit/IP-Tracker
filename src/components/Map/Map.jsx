ed466d39f62cfd7891e5b907444265aad8349c22
Eli Kovalev <elisey.k01@gmail.com>
2024-04-18 12:40:14 -0700

    fix lng

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,8 +7,8 @@ import './map.scss'
 
 const Map = ({coordinates}) => {
     //33.23107702737697, -117.25595722335544
-    console.log(Number(coordinates[0]))
-    const center = useMemo(() => ({ lat: Number(coordinates[0]), lng: Number(coordinates[1])}),[coordinates])
+    console.log(coordinates[0])
+    const center = useMemo(() => ({ lat: coordinates[0], lng: coordinates[1]}),[coordinates])
     
     const defMapOptions = {
         disableDefaultUI: true,
