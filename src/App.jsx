dbfd4a20dcba066986f1d8f41b2622d4181479c2
Eli Kovalev <elisey.k01@gmail.com>
2024-04-18 13:07:17 -0700

    fix lng#3

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Output from './components/Output/Output'
 import Map from './components/Map/Map'
 
 function App() {
-  const { isLoaded} = useLoadScript({ googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY})
+  const { isLoaded} = useLoadScript({ googleMapsApiKey: "AIzaSyByucCkPMord9BNSKKANxm2UmZrI0Lb8H0"})
   const [ip, setIp] = useState('185.169.0.44')
 
   // ip,type,status,location,area,city,asn,postcode,time
@@ -20,7 +20,7 @@ function App() {
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': import.meta.env.VITE_IP_GEO_API_KEY,
+		'X-RapidAPI-Key': "ca7ad9d72amsh0beacafc91a8fb6p1c4e25jsnf35a8d376821",
 		'X-RapidAPI-Host': 'ip-geo-location.p.rapidapi.com'
 	}
 };
